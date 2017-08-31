26f29b22a04885d0f992bfc6411f1ac9f67d6c0c
Laicing Yip <472285620@qq.com>
2017-08-31 15:09:16 +0800

    use monk

--- src/db.js
+++ src/db.js
@@ -1,28 +1,54 @@
-const mongoClient = require('mongodb').MongoClient
 const url = require('config').get('db').get('url')
+const mongo = require('mongodb')
+const monk = require('monk')
+const db2 = monk(url)
 
-const connection = async () => {
-    return await new Promise((resolve, reject) => {
-        mongoClient.connect(url, function (err, db) {
-            resolve(db);
-        })
-    })
-}
+const userCollection = db2.get('user')
 
 const insertUser = async (user) => {
+    const returnValue = await new Promise((resol, reject) => {
+        userCollection.insert(user, function (e, res) {
+            if (e) {
+                resol(false)
+                console.error(e)
+            }
+            else
+                resol(true)
+        })
+    })
+    return returnValue
+}
 
+const checkUser = async (phone) => {
+    const isUnique = await new Promise((resol, rej) => {
+        userCollection.find({ 'phone': phone }, (err, resp) => {
+            if (err) {
+                console.error(err)
+                resol(false)
+            }
+            else {
+                if (Array.isArray(resp) && resp.length != 0)
+                    resol(false)
+                else
+                    resol(true)
+            }
+        })
+    })
+    return isUnique
 }
 
 const check = async () => {
-    const db = await connection();
-    db.collection('user').find().toArray(function (e, docs) {
+    // userCollection.find({}, function (e, docs) {
+    //     console.log(docs)
+    // })
+    userCollection.find({}).then((docs) => {
         console.log(docs)
     })
-    db.close();
 }
 
 
 module.exports = {
     insertUser,
+    checkUser,
     check
 }
